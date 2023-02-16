009db8ce09005b28f4a657adf8caa7a2e021dd7f
Kevin Diesenberg <kdiesenb@gmail.com>
2023-02-16 12:38:38 -0500

    seeds transfered

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+# puts "🌱seeding..."
+
+# event1 =  Event.create( name: "Abc Camp", description: "really fun camp with lots of good instructors", location: "MWFF", price: 500, date: "12/12/2023", image: "https://www.skydivemag.com/new/wp-content/uploads/2022/01/Ragnarok-angle-flying-PC-Roy-Wimmer-Jaglom-@3-FI-860x484.jpg")
+# event2 = Event.create( name: "angle angle angle", description: 'just come have fun flying fast', location: "skydive orange", price: 500 , date: "11/02/1985", image: "https://www.skydivemag.com/new/wp-content/uploads/2022/01/Ragnarok-angle-flying-PC-Roy-Wimmer-Jaglom-@3-FI-860x484.jpg")
+# event3 = Event.create( name: "wangle wangle wangle", description: "we are gonna wangle and wangle and wangle", location: "skydive crosskeys", price: 500 , date: "07/07/07", image: "https://www.skydivemag.com/new/wp-content/uploads/2022/01/Ragnarok-angle-flying-PC-Roy-Wimmer-Jaglom-@3-FI-860x484.jpg")
+# event4 = Event.create( name: "Tummy time ", description: "um its all about tummy time.  show off your tummy skills and impress your coaches", location: "skydive chicage", price: 500, date: "nov 2nd 2025", image: "https://www.skydivemag.com/new/wp-content/uploads/2022/01/Ragnarok-angle-flying-PC-Roy-Wimmer-Jaglom-@3-FI-860x484.jpg")
+
+# user1 = User.create( email: "user@example.com", password_digest: "dfadf")
+# user2 = User.create( email: "email@example.com", password_digest: "fad")
+# user3 = User.create( email: "gmail@example.com", password_digest: "adsf")
+# user6 = User.create( email: "hotmail@example.com", password_digest: "dfadf")
+# user4 = User.create( email: "mike@example.com", password_digest: "dfadf")
+# user5 = User.create( email: "sally@example.com", password_digest: "fadfadf")
+
+# reg1 = Registration.create( first_name: "sara", last_name: "deeeeen", address: "56565 hunter baby way", city: "Port Huron", state: "michigan", number_of_jumps: 1, tunnel_time: 60, previous_camps: 10, ratings: "AFF-I, TI, coach", membership_number: "USPA c-123456", user_id: user1.id, event_id: event3.id)
+# reg2 = Registration.create( first_name: "polly", last_name: "dove", address: "56565 hunter baby way", city: "Port Huron", state: "michigan", number_of_jumps: 200, tunnel_time: 60, previous_camps: 10, ratings: "AFF-I, TI, coach", membership_number: "USPA c-123456", user_id: user2.id, event_id: event2.id)
+# reg3 = Registration.create( first_name: "lily", last_name: "greppins", address: "56565 hunter baby way", city: "Port Huron", state: "michigan", number_of_jumps: 200, tunnel_time: 60, previous_camps: 10, ratings: "AFF-I, TI, coach", membership_number: "USPA c-123456", user_id: user3.id, event_id: event1.id)
+# reg4 = Registration.create( first_name: "candac", last_name: "fren", address: "56565 hunter baby way", city: "Port Huron", state: "michigan", number_of_jumps: 200, tunnel_time: 60, previous_camps: 10, ratings: "AFF-I, TI, coach", membership_number: "USPA c-123456", user_id: user3.id, event_id: event1.id)
+# reg5 = Registration.create( first_name: "mike", last_name: "leamur", address: "56565 hunter baby way", city: "Port Huron", state: "michigan", number_of_jumps: 200, tunnel_time: 60, previous_camps: 10, ratings: "AFF-I, TI, coach", membership_number: "USPA c-123456", user_id: user4.id, event_id: event2.id)
+
+# inst1 = Instructor.create(first_name: "John Sue", last_name: "Mary Mike", about: "this is my about me for each instructor", image: "https://skydivemidwest.com/wp-content/uploads/2021/04/Angle-Flying.jpg" )
+# inst2 = Instructor.create(first_name: "John Sue", last_name: "Mary Mike", about: "this is my about me for each instructor", image: "https://skydivemidwest.com/wp-content/uploads/2021/04/Angle-Flying.jpg" )
+# inst3 = Instructor.create(first_name: "John Sue", last_name: "Mary Mike", about: "this is my about me for each instructor", image: "https://skydivemidwest.com/wp-content/uploads/2021/04/Angle-Flying.jpg" )
+# inst4 = Instructor.create(first_name: "John Sue", last_name: "Mary Mike", about: "this is my about me for each instructor", image: "https://skydivemidwest.com/wp-content/uploads/2021/04/Angle-Flying.jpg" )
+
+# puts "✅ seeding"
